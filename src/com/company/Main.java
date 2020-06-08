56f55027ff3d4d5f472b32a8d57797a29b96dd83
Bruno Ferreira <bruno.ferreirasagomes@gmail.com>
2020-06-08 13:27:37 +0200

    add ms timer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,13 +8,18 @@ public class Main {
     public static void main(String[] args) {
         Quicksort qs = new Quicksort();
 
-        int range = 50;
-        int length = 10;
+        int range = 1000000;
+        int length = 10000;
+
 
         int[] listToSort = qs.makeList(range,length);
         System.out.println(Arrays.toString(listToSort));
 
+        long tstart = System.currentTimeMillis();
         qs.quickSort(listToSort,0,length-1);
+        long tend = System.currentTimeMillis();
         System.out.println(Arrays.toString(listToSort));
+        long time = tend - tstart;
+        System.out.println("time (ms):" + time );
     }
 }
